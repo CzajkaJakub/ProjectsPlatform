a34e4ae4f8220850bca16d71a272588b6e7bbb74
Jakub Czajka <CzajkaPL@o2.pl>
2022-05-11 10:54:14 +0200

    Migrated database to postgresql

--- src/main/java/com/example/simpleprojectsplatform/TheCountriesWiki/SqlConnection.java
+++ src/main/java/com/example/simpleprojectsplatform/TheCountriesWiki/SqlConnection.java
@@ -20,9 +20,9 @@ public class SqlConnection implements PathsTitlesFiles {
     public void makeConnection(ImageView connectionMark, Button makeConnectionButton, Button searchButton){
         connection = null;
         try {
-            String user = "czajkaja_countries";
-            String password = "countries123";
-            String url = "jdbc:mariadb://hs10.linux.pl:3306/czajkaja_mybase";
+            String user = "jqouqvkcbhhjhf";
+            String password = "f55dd258144a7eefa3c8cfa22c40aa8dfd466ccfdee2839bdb2dd45e826248c3";
+            String url = "jdbc:postgresql://ec2-3-230-122-20.compute-1.amazonaws.com:5432/d2ttasfjtidlfl";
             connectionMark.setImage(connectionYesIcon);
             connection = DriverManager.getConnection(url, user, password);
             connected = ConnectionStatus.CONNECTED;
@@ -38,7 +38,7 @@ public class SqlConnection implements PathsTitlesFiles {
 
         ArrayList<String> data = new ArrayList<>();
         String query = String.format(
-                "SELECT `iso`, `Nice name`, `iso3`, `Num code`, `Phone code` FROM country WHERE `%s` = '%s'",
+                "select \"Iso\", \"Nice name\", iso3, \"Num code\", \"Phone code\" from country where \"%s\" = '%s'",
                 searchByType, searchValInput);
 
         try {
